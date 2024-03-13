abf8f56544644fb22ea7361e1eaf42b4e5ff213c
ANSHyeon <hyeonan1220@gmail.com>
2024-03-13 16:13:09 +0900

    chore: Coil 라이브러리 사용을 위한 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -116,4 +116,7 @@ dependencies {
     implementation(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
+
+    //Coil
+    implementation("io.coil-kt:coil-compose:2.6.0")
 }
