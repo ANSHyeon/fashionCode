4f5d02aa332a048fe29cca02c833f8fcfc843e33
ANSHyeon <hyeonan1220@gmail.com>
2024-03-14 16:02:11 +0900

    feat: 커뮤니티 작성화면, 이미지 선택 기능 구현

--- app/src/main/java/com/anshyeon/fashioncode/ui/screen/home/community/write/CommunityWriteScreen.kt
+++ app/src/main/java/com/anshyeon/fashioncode/ui/screen/home/community/write/CommunityWriteScreen.kt
@@ -1,19 +1,28 @@
 package com.anshyeon.fashioncode.ui.screen.home.community.write
 
 import android.net.Uri
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,6 +35,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import com.anshyeon.fashioncode.R
@@ -36,6 +46,16 @@ import com.anshyeon.fashioncode.ui.theme.gray
 fun CommunityWriteScreen(navController: NavHostController) {
 
     val viewModel: CommunityWriteViewModel = hiltViewModel()
+    val selectedImageList by viewModel.selectedImageList.collectAsStateWithLifecycle()
+
+    val multiplePhotoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetMultipleContents(),
+        onResult = { uris ->
+            if (selectedImageList.size + uris.size <= 10) {
+                viewModel.addImageUris(uris)
+            }
+        }
+    )
 
     Box(
         modifier = Modifier.fillMaxSize()
@@ -44,12 +64,37 @@ fun CommunityWriteScreen(navController: NavHostController) {
             BackButtonAppBar(title = stringResource(id = R.string.label_app_bar_community_write)) {
                 viewModel.navigateBack(navController)
             }
+            Spacer(modifier = Modifier.height(17.dp))
+            ImageSelector(selectedImageList, multiplePhotoPickerLauncher) { index ->
+                viewModel.removeImageUris(index)
+            }
         }
     }
 }
 
 @Composable
-private fun ImageSelector(selectedImageCount: Int, onclick: () -> Unit) {
+private fun ImageSelector(
+    selectedImageList: List<Uri>,
+    multiplePhotoPickerLauncher: ManagedActivityResultLauncher<String, List<@JvmSuppressWildcards Uri>>,
+    onclick: (index: Int) -> Unit
+) {
+    Row {
+        GalleryImageSelector(selectedImageList.size) {
+            multiplePhotoPickerLauncher.launch("image/*")
+        }
+        Spacer(modifier = Modifier.size(10.dp))
+        LazyRow {
+            itemsIndexed(selectedImageList) { index, imageUri ->
+                SelectedImage(imageUri) {
+                    onclick(index)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun GalleryImageSelector(selectedImageCount: Int, onclick: () -> Unit) {
     Box(
         modifier = Modifier
             .padding(start = 24.dp, top = 27.dp)
--- app/src/main/java/com/anshyeon/fashioncode/ui/screen/home/community/write/CommunityWriteViewModel.kt
+++ app/src/main/java/com/anshyeon/fashioncode/ui/screen/home/community/write/CommunityWriteViewModel.kt
@@ -1,13 +1,32 @@
 package com.anshyeon.fashioncode.ui.screen.home.community.write
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavHostController
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class CommunityWriteViewModel @Inject constructor(
 ) : ViewModel() {
+
+    private val _selectedImageList = MutableStateFlow(emptyList<Uri>())
+    val selectedImageList: StateFlow<List<Uri>> = _selectedImageList
+
+    fun addImageUris(imageUris: List<Uri>) {
+        _selectedImageList.value = mutableListOf<Uri>().apply {
+            addAll(_selectedImageList.value)
+            addAll(imageUris)
+        }
+    }
+
+    fun removeImageUris(index: Int) {
+        _selectedImageList.value = _selectedImageList.value.toMutableList().apply {
+            removeAt(index)
+        }.toList()
+    }
 
     fun navigateBack(navController: NavHostController) {
         navController.popBackStack()
