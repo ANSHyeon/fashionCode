8fc8fd970d18207ad7d1b20d680a6b562e8f6adc
ANSHyeon <hyeonan1220@gmail.com>
2024-03-29 16:29:00 +0900

    feat: 스타일 상세화면, 기본적인 UI와 viewModel 구현

---
+++ app/src/main/java/com/anshyeon/fashioncode/ui/screen/home/style/detail/StyleDetailScreen.kt
@@ -0,0 +1,60 @@
+package com.anshyeon.fashioncode.ui.screen.home.style.detail
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
+import com.anshyeon.fashioncode.R
+import com.anshyeon.fashioncode.ui.component.appBar.BackButtonAppBar
+import com.anshyeon.fashioncode.ui.component.loadingView.LoadingView
+import com.anshyeon.fashioncode.ui.component.snackBar.TextSnackBarContainer
+
+@Composable
+fun CommunityDetailScreen(navController: NavHostController) {
+
+    val viewModel: StyleDetailViewModel = hiltViewModel()
+
+    val scrollState = rememberScrollState()
+
+    val isLoadingState by viewModel.isLoading.collectAsStateWithLifecycle()
+    val snackBarTextState by viewModel.snackBarText.collectAsStateWithLifecycle()
+    val showSnackBarState by viewModel.showSnackBar.collectAsStateWithLifecycle()
+
+    Scaffold(
+        topBar = {
+            BackButtonAppBar(stringResource(id = R.string.label_app_bar_community_detail)) {
+                viewModel.navigateBack(navController)
+            }
+        }
+    ) {
+        TextSnackBarContainer(
+            snackbarText = snackBarTextState,
+            showSnackbar = showSnackBarState,
+            onDismissSnackbar = { viewModel.dismissSnackBar() }
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it)
+                    .padding(15.dp)
+                    .verticalScroll(scrollState)
+            ) {
+
+            }
+            LoadingView(
+                isLoading = isLoadingState
+            )
+
+        }
+    }
+}
