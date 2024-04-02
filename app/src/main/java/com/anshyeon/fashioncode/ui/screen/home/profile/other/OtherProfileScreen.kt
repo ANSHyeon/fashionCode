9551b1fc05ab0417102c1145f678da3d5fb09aac
ANSHyeon <hyeonan1220@gmail.com>
2024-04-02 17:23:17 +0900

    feat: 타 유저 프로필 화면, 기본적인 UI 구현

---
+++ app/src/main/java/com/anshyeon/fashioncode/ui/screen/home/profile/other/OtherProfileScreen.kt
@@ -0,0 +1,45 @@
+package com.anshyeon.fashioncode.ui.screen.home.profile.other
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
+import com.anshyeon.fashioncode.ui.component.appBar.BackButtonAppBar
+import com.anshyeon.fashioncode.ui.component.loadingView.LoadingView
+import com.anshyeon.fashioncode.ui.component.snackBar.TextSnackBarContainer
+
+@Composable
+fun OtherProfileScreen(navController: NavHostController, userId: String) {
+
+    val viewModel: OtherProfileViewModel = hiltViewModel()
+
+    val isLoadingState by viewModel.isLoading.collectAsStateWithLifecycle()
+    val snackBarTextState by viewModel.snackBarText.collectAsStateWithLifecycle()
+    val showSnackBarState by viewModel.showSnackBar.collectAsStateWithLifecycle()
+
+    Scaffold(
+        topBar = {
+            BackButtonAppBar("") {
+                viewModel.navigateBack(navController)
+            }
+        }
+    ) {
+        TextSnackBarContainer(
+            snackbarText = snackBarTextState,
+            showSnackbar = showSnackBarState,
+            onDismissSnackbar = { viewModel.dismissSnackBar() }
+        ) {
+
+            Box(modifier = Modifier.padding(it))
+
+            LoadingView(
+                isLoading = isLoadingState
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/anshyeon/fashioncode/ui/screen/home/profile/other/OtherProfileViewModel.kt
@@ -0,0 +1,30 @@
+package com.anshyeon.fashioncode.ui.screen.home.profile.other
+
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavHostController
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class OtherProfileViewModel @Inject constructor(
+) : ViewModel() {
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    private val _snackBarText = MutableStateFlow("")
+    val snackBarText: StateFlow<String> = _snackBarText
+
+    private val _showSnackBar = MutableStateFlow(false)
+    val showSnackBar: StateFlow<Boolean> = _showSnackBar
+
+    fun dismissSnackBar() {
+        _showSnackBar.value = false
+    }
+
+    fun navigateBack(navController: NavHostController) {
+        navController.popBackStack()
+    }
+}
