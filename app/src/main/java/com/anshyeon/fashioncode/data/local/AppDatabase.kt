642c06f792e48b6444453549d98a502925240c11
ANSHyeon <hyeonan1220@gmail.com>
2024-04-15 16:56:38 +0900

    feat: StyleDao 추가

--- app/src/main/java/com/anshyeon/fashioncode/data/local/AppDatabase.kt
+++ app/src/main/java/com/anshyeon/fashioncode/data/local/AppDatabase.kt
@@ -4,12 +4,15 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.anshyeon.fashioncode.data.local.dao.ClothesDao
+import com.anshyeon.fashioncode.data.local.dao.StyleDao
 import com.anshyeon.fashioncode.data.model.Clothes
 import com.anshyeon.fashioncode.data.model.Converters
+import com.anshyeon.fashioncode.data.model.LocalStyle
 
-@Database(entities = [Clothes::class], version = 1)
+@Database(entities = [Clothes::class, LocalStyle::class], version = 1)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun clothesDao(): ClothesDao
+    abstract fun stylesDao(): StyleDao
 }
